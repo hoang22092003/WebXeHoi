82207c87c1212125c9acd1afd6ae44f6f59e1b0c
AnhNguyen <nd.anh@hutech.edu.vn>
2024-03-20 12:53:27 +0700

    Add project files.

---
+++ WebXeHoi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebXeHoi.Repositories;
+
+namespace WebXeHoi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // Hiển thị danh sách sản phẩm
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+    }
+}
---
+++ WebXeHoi/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebXeHoi.Models;
+
+namespace WebXeHoi.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
